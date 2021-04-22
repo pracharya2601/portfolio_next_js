ec2e436d9771e382e7d498541692fa32ac0e3dfa
unknown <pracharya2601@gmail.com>
2021-04-22 09:05:53 -0700

    cleanup backend

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import { useRouter } from "next/router";
 import GlobalTheme from 'context/theme';
-import { UserProvider } from '@auth0/nextjs-auth0';
 import { AnimatePresence } from "framer-motion";
 
 import useScrollPosition from 'hooks/useScrollPosition';
@@ -32,11 +31,9 @@ function MyApp({ Component, pageProps }) {
   const router = useRouter();
   return (
       <AnimatePresence exitBeforeEnter onExitComplete={handExitComplete}>
-        <UserProvider>
           <GlobalTheme>
             <Component {...pageProps}  key={router.route}/>
           </GlobalTheme>
-        </UserProvider>
       </AnimatePresence>
     )
 }
